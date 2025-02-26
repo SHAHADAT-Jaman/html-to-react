10a0a9ed750f443075d7b0e05a712288e50ecf62
Shahadat <shahadatjaman4975@gmail.com>
2025-02-26 21:47:07 +0600

    changes in blog page

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -7,7 +7,7 @@ function Blog() {
                <h2>Our Blogs</h2>
                <div className="posts">
                     <article className="post">
-                         <h3>How to work with prototype design with adobe xd featuring tools</h3>
+                         <h3>How to work with prototype design with adobe xd featuring tools.</h3>
                          <p>The 2019 Innovation by Design Awards honor the designers and businesses solving the problems of today and tomorrow. It is one of the most sought-after design...</p>
                          <a href="#">Read More</a>
                     </article>
